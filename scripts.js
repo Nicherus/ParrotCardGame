6a7e91d75a01684a6e059f7ff42e4a693392806f
nicherus <joaonichev@gmail.com>
2020-09-09 21:02:06 -0300

    cria cards (nao aleatorio e todas imagens iguais)

--- scripts.js
+++ scripts.js
@@ -1,9 +1,33 @@
-//var numCards = prompt("Com quantas cartas queres jogar? (apenas números pares de 4 a 12)");
+padrao();
 
-testaNumCards();
+function padrao(){
+    
+    var numCards = prompt("Com quantas cartas queres jogar? (apenas números pares de 4 a 12)");
+    
+    var teste = testaNumCards(numCards);
+    
+    if(teste === 1){
+        createCards(numCards);
+    }
+    else{
+        alert("NUMERO ERRADO")
+        padrao();
+    } 
+}
+
+function createCards(numCards){
+    for(var i = 0; i < numCards; i++){
+        var newLi = document.createElement("li");
+        newLi.setAttribute('class', 'card');
+        newLi.innerHTML = 
+        "<div class='front-face'> <img class='front-img' src='imagens/front.png'> </div> <div class='back-face'><img class='back-img' src='imagens/fiestaparrot.gif'></div>";
+        var lista = document.querySelector("ul");
+        lista.appendChild(newLi);
+    }
+}
 
 
-function testaNumCards(){
+function testaNumCards(numCards){
     if(((numCards > 3) && (numCards < 13)) && ((numCards%2) == 0)){
         return 1;
     }
@@ -11,3 +35,14 @@ function testaNumCards(){
         return 0;
     }
 }
+
+
+/* 
+    imagens/bobrossparrot.gif
+    imagens/explodyparrot.gif
+    imagens/fiestaparrot.gif
+    imagens/metalparrot.gif
+    imagens/revertitparrot.gif
+    imagens/tripletsparrot.gif
+    imagens/unicornparrot.gif
+*/
